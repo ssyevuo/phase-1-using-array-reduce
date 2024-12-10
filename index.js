fe9665bbf266c46de0eff888a7f670b7d79a7034
Shakira Syevuo <shakirasyevuo@gmail.com>
2024-12-11 01:04:59 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const totalBatteries = batteryBatches.reduce((sum, number) => sum + number, 0);
+//0 works as the initial value for the sum to know where to start from before looping through the batteryBatches.
+//number represents each value in the batteryBatches array that is added tothe sum to get the total
+
+console.log(totalBatteries)
+
+
